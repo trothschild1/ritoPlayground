003fb0eb0831915e4f3721187a9e4d8255979455
Teddy Rothschild <teddy.rothschild@gmail.com>
2025-07-15 11:57:55 -0700

    remove routing logic

--- src/app/compare/Compare.tsx
+++ src/app/compare/Compare.tsx
@@ -1,19 +1,14 @@
 "use client";
 
 import React from "react";
-import { usePathname } from "next/navigation";
 import { Header } from "../../shared/components/Header";
 import { ChampGrid } from "./components/ChampGrid";
 import { Comparisons } from "./components/Comparisons";
 
 export const Compare = () => {
-  const pathname = usePathname();
-  const routeName = pathname.split("/").pop() || "compareisfjd";
-  const page = routeName.charAt(0).toUpperCase() + routeName.slice(1);
-
   return (
     <>
-      <Header page={page} />
+      <Header />
       <Comparisons />
       <ChampGrid />
     </>
