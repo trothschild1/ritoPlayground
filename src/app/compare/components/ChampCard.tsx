937d0d4f53aff23bba8dc00f03c74386e2383f5a
Teddy Rothschild <teddy.rothschild@gmail.com>
2025-07-15 10:05:30 -0700

    ChampCard component and styling

---
+++ src/app/compare/components/ChampCard.tsx
@@ -0,0 +1,37 @@
+import { Card, Flex, Text } from "@radix-ui/themes";
+import { CirclePlus } from "lucide-react";
+import React from "react";
+import "./ChampCard.css";
+
+export const ChampCard = ({
+  id,
+  name,
+  image_splash,
+  title,
+  height,
+  width
+}: {
+  id: string;
+  name: string;
+  image_splash?: string;
+  title?: string;
+  height: number;
+  width: number;
+}) => {
+  return (
+    <Card key={id} className="card">
+      <Flex align="center" gap="2">
+        <img
+          className="card-icon"
+          src={`/tiles/${image_splash}`}
+          alt={name}
+          width={width}
+          height={height}
+        />
+        <Text className="card-name">{name}</Text>
+        <Text className="card-title">{title}</Text>
+        <CirclePlus size={16} className="card-add-icon" />
+      </Flex>
+    </Card>
+  );
+};
